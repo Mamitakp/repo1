02ff0416d4b6af610f9b3b49ec9f53f7981d0675
Your Name <mamitakp>
2022-05-09 02:39:48 -0700

    f9

---
+++ f9.py
@@ -0,0 +1,18 @@
+list1=[]
+x=input('enter user name')
+y= input('enter user id')
+dict1={'name':x,'id':y }
+x=input('enter user name')
+y= input('enter user id')
+dict2={'name':x,'id':y }
+list1.append(dict1)
+list1.append(dict2)
+print(list1)
+
+
+
+
+
+ 
+
+   
